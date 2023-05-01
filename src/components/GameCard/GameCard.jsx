4b4c4b4f454ed3affc5e33e245a1262231389618
Pablo Villedae <pablov.e12345@gmail.com>
2023-05-01 14:53:55 -0700

    add game card review

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,5 +1,12 @@
+import GameReview from "../GameReview/GameReview";
+
 const GameCard = (props) => {
-  return ( <h1>This is GameCard Component</h1> );
+  return ( 
+    <>
+      <h1>This is GameCard Component</h1>
+      <GameReview/>
+    </>
+  );
 }
- 
+
 export default GameCard;
---
+++ src/components/GameReview/GameReview.jsx
@@ -0,0 +1,5 @@
+const GameReview = (props) => {
+  return ( <p>This is a game review component</p> );
+}
+
+export default GameReview;
