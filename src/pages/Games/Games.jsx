8046444a925e696fa3306fa3265cbe1dfda5f185
Pablo Villedae <pablov.e12345@gmail.com>
2023-05-11 14:19:16 -0700

    styled button on game list

--- src/pages/Games/Games.jsx
+++ src/pages/Games/Games.jsx
@@ -5,9 +5,12 @@ const Games = (props) => {
   return (
     <>
       <h1>Games List</h1>
-      <p><button><a href="/games/ttt">TicTacToe</a></button></p>
-      <p><button><a href="/games/guess">Guess The Number</a></button></p>
-      <p><button><a href="/games/hangMan">HangMan</a></button></p>
+    <div className={styles.list}>
+
+      <p ><button className={styles.gameList} ><a href="/games/ttt">TicTacToe</a></button></p>
+      <p  ><button className={styles.gameList}><a href="/games/guess">Guess The Number</a></button></p>
+      <p  ><button className={styles.gameList} ><a href="/games/hangMan">HangMan</a></button></p>
+    </div>
     </>
 
     );
