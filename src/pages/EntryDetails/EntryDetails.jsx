07404a9c96c9fb219c790f25b3f80ba696c594e1
Pablo Villedae <pablov.e12345@gmail.com>
2023-05-16 12:55:17 -0700

    add show effect and entry details page

--- src/pages/EntryDetails/EntryDetails.jsx
+++ src/pages/EntryDetails/EntryDetails.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useState } from "react"
+import * as entryService from '../../services/entryService'
+
 const EntryDetails = (props) => {
-  return ( <h1>This is entry details</h1> );
-}
- 
+
+  const [entry, setEntry] = useState(null)
+  const {id} = useParams
+  useEffect(() => {
+    const fetchExperience = async () => {
+      const data = await entryService.show(id)
+      setEntry(data)
+    }
+    fetchExperience()
+  }, [id])
+
+
+  return (
+    <>
+      <h1>This is entry details</h1> 
+      <p>{entry.mood}</p>
+    </>
+    );
+} 
 export default EntryDetails;
