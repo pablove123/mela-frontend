cfbd2d71b59dd4a10a1e5586334dbf792f07b64e
Pablo Villedae <pablov.e12345@gmail.com>
2023-05-24 14:04:32 -0700

    styling guess the number game

--- src/pages/GuessTheNumber/GuessTheNumber.jsx
+++ src/pages/GuessTheNumber/GuessTheNumber.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import styles from "../GuessTheNumber/GuessTheNumber.module.css"
 
 function GuessTheNumber() {
   const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1);
   const [guess, setGuess] = useState('');
+  const [tries, setTries] = useState('0');
   const [message, setMessage] = useState('Guess a number between 1 and 100');
 
   const [currentGuess, setCurrentGuess] = useState('')
@@ -15,9 +17,11 @@ function GuessTheNumber() {
     } else if (userGuess < 1 || userGuess > 100) {
       setMessage('Please enter a number between 1 and 100');
     } else if (userGuess === number) {
-      setMessage(`Congratulations! You guessed the number ${number}!`);
+      setMessage(`Congratulations! You guessed the number ${number}!`)
+      ;
     } else if (userGuess < number) {
-      setMessage('The number is higher');
+      setMessage('The number is higher')
+      setTries();
     } else if (userGuess > number) {
       setMessage('The number is lower');
     }
@@ -28,9 +32,9 @@ function GuessTheNumber() {
   return (
     <div>
       <h1>Guess the Number Game</h1>
-      <p>{message}</p>
-      <p>Previous Guess: {currentGuess}</p>
-      <form onSubmit={handleGuess}>
+      <p className={styles.message}>{message}</p>
+      <p className={styles.message}>Previous Guess: {currentGuess}</p>
+      <form className={styles.message} onSubmit={handleGuess}>
         <input
           type="number"
           value={guess}
@@ -38,6 +42,7 @@ function GuessTheNumber() {
         />
         <button type="submit">Guess</button>
       </form>
+      <h2>It took you {tries} guesses </h2>
     </div>
   );
 }
